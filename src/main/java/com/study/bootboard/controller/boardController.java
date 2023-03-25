d9bfee9087b8e2c335da0a6677368b778f2bf3d9
AhnYeonghoo <dksdudgn00@naver.com>
2023-03-25 15:06:26 +0900

    mariaDb 스키마 생성 및 DB 연결

---
+++ src/main/java/com/study/bootboard/controller/boardController.java
@@ -0,0 +1,15 @@
+package com.study.bootboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class boardController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String main() {
+        return "Hello World";
+    }
+}
